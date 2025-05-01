c4b881d49769043e0fee083ed35cdd71949c4b0b
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-01 15:06:50 -0300

    feat(structure): organizes the modules and configures the initial routes

---
+++ navega-app/src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginLayoutComponent,
+    DashboardLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
