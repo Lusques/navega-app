48210f90f044b90ede2e0050f6946fa973252ef0
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-02 00:26:39 -0300

    feat(auth): create password field with PrimeNG

--- navega-app/src/app/shared/components/form/input-password/input-password.component.ts
+++ navega-app/src/app/shared/components/form/input-password/input-password.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-password',
   templateUrl: './input-password.component.html',
-  styleUrls: ['./input-password.component.less']
+  styleUrls: ['./input-password.component.less'],
 })
 export class InputPasswordComponent implements OnInit {
+  password: string = '';
+  toggleMask: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- navega-app/src/app/shared/shared.module.ts
+++ navega-app/src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { InputAuthIdentifierComponent } from './components/form/input-auth-identifier/input-auth-identifier.component';
 import { InputPasswordComponent } from './components/form/input-password/input-password.component';
 import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [InputAuthIdentifierComponent, InputPasswordComponent],
-  imports: [CommonModule, InputTextModule],
-  exports: [InputAuthIdentifierComponent],
+  imports: [CommonModule, FormsModule, InputTextModule, PasswordModule],
+  exports: [InputAuthIdentifierComponent, InputPasswordComponent],
 })
 export class SharedModule {}
