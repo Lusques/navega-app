563645825cb8fd5db12b6bebfdec19720b0f6a9e
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-01 22:47:14 -0300

    feat(auth): ends login screen (structure without PrimeNG)

--- navega-app/src/app/app.module.ts
+++ navega-app/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './features/dashboard/dashboard.module';
+import { AuthModule } from './features/auth/auth.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DashboardModule
+    AuthModule,
+    CoreModule,
+    DashboardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- navega-app/src/app/core/core.module.ts
+++ navega-app/src/app/core/core.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { Router, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginLayoutComponent, DashboardLayoutComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
+  exports: [RouterModule]
 })
 export class CoreModule {}
