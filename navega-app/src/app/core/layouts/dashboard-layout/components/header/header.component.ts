19514ea9e77bf886c99398ee79fadf49f8487e6a
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-02 15:23:09 -0300

    feat(dashboard): adds animation to navigation menu

--- navega-app/src/app/core/layouts/dashboard-layout/components/header/header.component.ts
+++ navega-app/src/app/core/layouts/dashboard-layout/components/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less']
+  styleUrls: ['./header.component.less'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() headerClicked = new EventEmitter<void>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  notifyClick() {
+    this.headerClicked.emit();
   }
-
 }
--- navega-app/src/app/core/layouts/dashboard-layout/dashboard-layout.component.ts
+++ navega-app/src/app/core/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard-layout',
   templateUrl: './dashboard-layout.component.html',
-  styleUrls: ['./dashboard-layout.component.less']
+  styleUrls: ['./dashboard-layout.component.less'],
 })
 export class DashboardLayoutComponent implements OnInit {
+  showNavigation: boolean = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  toggleNavigation() {
+    this.showNavigation = !this.showNavigation;
   }
-
 }
