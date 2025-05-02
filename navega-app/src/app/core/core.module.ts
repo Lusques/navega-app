2cff225b40deb7c61d9b252c2ea87acd721ab974
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-02 11:27:17 -0300

    feat(layout): creates dashboard header component

--- navega-app/src/app/core/core.module.ts
+++ navega-app/src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 import { Router, RouterModule } from '@angular/router';
+import { HeaderComponent } from './layouts/dashboard-layout/components/header/header.component';
 
 @NgModule({
-  declarations: [LoginLayoutComponent, DashboardLayoutComponent],
+  declarations: [LoginLayoutComponent, DashboardLayoutComponent, HeaderComponent],
   imports: [CommonModule, RouterModule],
   exports: [RouterModule]
 })
