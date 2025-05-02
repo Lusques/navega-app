701ea4a64317008c3d3ed5159f02e7de7dcdeb56
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-02 15:58:14 -0300

    style(nav): fine-tuning navigation styling

--- navega-app/src/app/core/layouts/dashboard-layout/components/navigation/navigation.component.ts
+++ navega-app/src/app/core/layouts/dashboard-layout/components/navigation/navigation.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 
+interface navItem {
+  icon: string;
+  alt?: string;
+  text: string;
+  path: string;
+}
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.less']
+  styleUrls: ['./navigation.component.less'],
 })
 export class NavigationComponent implements OnInit {
+  sidebarItems: navItem[] = [
+    {
+      icon: '/assets/icons/file-invoice-dollar.svg',
+      text: 'Ver extrato',
+      path: '',
+    },
+    {
+      icon: '/assets/icons/envelope-open-dollar.svg',
+      text: 'Contribuição Mensal',
+      path: '',
+    },
+    {
+      icon: '/assets/icons/sack-dollar.svg',
+      text: 'Contribuição Extra',
+      path: '',
+    },
+    {
+      icon: '/assets/icons/file-alt.svg',
+      text: 'Documentos',
+      path: '',
+    },
+    {
+      icon: '/assets/icons/user-chart.svg',
+      text: 'Regime de Tributação',
+      path: '',
+    },
+    {
+      icon: '/assets/icons/comment-dollar.svg',
+      text: 'Solicitar Benefício',
+      path: '',
+    },
+    {
+      icon: '/assets/icons/file-chart-line.svg',
+      text: 'Extrato Regressivo',
+      path: '',
+    },
+    {
+      icon: '/assets/icons/info.svg',
+      text: 'Informações',
+      path: '',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
