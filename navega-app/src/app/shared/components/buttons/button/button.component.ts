a836b12a08d3babc7263520c5656f5aa986598eb
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-03 14:49:59 -0300

    feat(shared): completes accordion component

--- navega-app/src/app/shared/components/buttons/button/button.component.ts
+++ navega-app/src/app/shared/components/buttons/button/button.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+type variants = 'default' | 'ghost';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.less']
+  styleUrls: ['./button.component.less'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() styleClass: string = '';
+  @Input() label: string = 'label';
+  @Input() variant: variants = 'default';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getVariant() {
+    return this.variant == 'default'
+      ? ' button__component--default'
+      : ' button__component--ghost';
   }
-
 }
